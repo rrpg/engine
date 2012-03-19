187805934dfbf8f106369866388099e3964cbded
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-03-19 17:48:04 +0100

    includes added and namespace setted

--- CommandFactory.h
+++ CommandFactory.h
@@ -1,8 +1,12 @@
 #ifndef DEF_COMMAND_FACTORY
 #define DEF_COMMAND_FACTORY
 
+#include <string>
+#include <vector>
 #include "Player.h"
 #include "CommandAbstract.h"
+
+using namespace std;
 
 class CommandFactory
 {
