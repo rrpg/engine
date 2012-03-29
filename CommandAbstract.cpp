8ed7c5ea04118e743a3abffb30c028e186e83638
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-03-29 21:43:29 +0200

    attributes and setters added

--- CommandAbstract.cpp
+++ CommandAbstract.cpp
@@ -2,8 +2,21 @@
 //
 
 #include "CommandAbstract.h"
+#include "Player.h"
+#include <vector>
+#include <string>
 
 CommandAbstract::CommandAbstract()
 {
 
+}
+
+void CommandAbstract::setArgs(std::vector <std::string> args)
+{
+    _args = args;
+}
+
+void CommandAbstract::setPlayer(Player* &player)
+{
+    _player = player;
 }
