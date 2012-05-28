850809e50104f57a09923dab51d3238dc86fced1
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-05-28 12:15:14 +0200

    script to recreate the DB added

---
+++ database/createDb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "rm existing db"
+rm rpg.db
+
+echo "creating db structure"
+sqlite3 -init structure.sql rpg.db '.quit'
+
+echo "inserting db values"
+sqlite3 -init values.sql rpg.db '.quit'
+
