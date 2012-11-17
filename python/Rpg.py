f5b56e603882c648f8de37d98876269808b8534c
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-11-17 17:58:23 +0100

    comment added

--- python/Rpg.py
+++ python/Rpg.py
@@ -5,6 +5,7 @@ from CommandFactory import CommandFactory
 
 class Rpg:
     def __init__(self, login, password, action):
+        #if the game is launched with login/password, the player is directly fetched
         if login != None and password != None:
             print 'rpg created for user ' + login
             print password
