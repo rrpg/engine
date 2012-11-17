526221b52975842ed5e7e85ff311ba8e4be558d0
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-11-17 18:13:48 +0100

    PlayerModel methods added

--- python/PlayerModel.py
+++ python/PlayerModel.py
@@ -5,5 +5,38 @@ from Model import Model
 import datetime
 
 class PlayerModel(CharacterModel):
-    pass
+    def __init__(self):
+        self._playerFields = {}
+
+    @staticmethod
+    def loadByLoginAndPassword(login, password):
+        query = "\
+            SELECT\
+                id_player,\
+                login\
+            FROM\
+                player\
+            WHERE\
+                login = ? AND password = ?\
+        "
+
+        model = Model.fetchOneRow(query, (login, password))
+
+        if len(model) > 0:
+            pm = PlayerModel()
+            pm._setPk(model[0])
+            pm.setLogin(model[1])
+            return pm
+
+        return None
+
+    def _setPk(self, pk):
+        self._playerFields["id_player"] = pk;
+
+    def setLogin(self, login):
+        self._playerFields["login"] = login;
+
+    def setPassword(self, password):
+        self._playerFields["password"] = password;
+
 
