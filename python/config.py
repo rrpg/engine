b10723eae12b11deed2b609a4da9c949e4bde456
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-11-17 18:02:20 +0100

    config.py added

---
+++ python/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+
+server = {}
+server['hostname'] = 'localhost'
+server['port'] = 4249
+
+server['services'] = {}
+server['services']['auth'] = '/auth'
+
+db = '../database/rpg.db'
