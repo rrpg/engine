a891a6f2534158dc95b966c094bf52bbe195cc1a
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-11-17 17:58:56 +0100

    __init__ body added

--- python/Rpg.py
+++ python/Rpg.py
@@ -7,6 +7,12 @@ class Rpg:
     def __init__(self, login, password, action):
         #if the game is launched with login/password, the player is directly fetched
         if login != None and password != None:
-            print 'rpg created for user ' + login
-            print password
-            print action
+            self._player = Player(login, password)
+            self._authenticate = True
+        #else an empty player is created
+        else:
+            self._player = Player(None, None)
+            self._authenticate = False
+
+        self._action = action
+
