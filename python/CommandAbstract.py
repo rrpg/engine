afe750928e75f0bca166f5bf6d2a84fa510e1e87
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2012-12-19 18:05:51 +0100

    pseudo "Abstract" class creates -- @TODO: must be a true abstract class

---
+++ python/CommandAbstract.py
@@ -0,0 +1,10 @@
+# -*- coding: utf8 -*-
+
+#~ Abstract class ?
+class CommandAbstract():
+    def setArgs(self, args):
+        self._args = args
+
+    def setPlayer(self, player):
+        self._player = player
+
