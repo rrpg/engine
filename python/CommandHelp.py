67e3956006664f778b9dbc1929b3bdbd5e3e37ab
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-02-03 22:16:13 +0100

    help command class added

---
+++ python/CommandHelp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf8 -*-
+
+from CommandAbstract import CommandAbstract
+from CommandException import CommandException
+
+class CommandHelp(CommandAbstract):
+    def run(self):
+        print 'Available commands:'
+        print 'talk <Character name> "<Sentence>": Talk to a character'
+        print 'createPlayer: Not Yet Implemented'
+        print 'help: Display this help'
+        print 'quit|exit|q: Quit the game'
+
