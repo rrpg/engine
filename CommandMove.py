696bc43cc4101b0ea139ee9044499e671dddc804
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-03-17 23:21:43 +0100

    move command activated

--- CommandMove.py
+++ CommandMove.py
@@ -2,6 +2,7 @@
 
 from CommandAbstract import CommandAbstract
 from CommandException import CommandException
+from Area import Area
 
 
 class CommandMove(CommandAbstract):
@@ -13,4 +14,13 @@ class CommandMove(CommandAbstract):
         if direction not in ("north", "east", "south", "west"):
             raise CommandException("%s is not a valid direction" % direction)
 
+        area = Area.getByIdCHaracterAndDirection(
+            self._player._model.getIdCharacter(), direction
+        )
+
+        if area is None:
+            raise CommandException('I cannot go there')
+        else:
+            self._player.goTo(area._model.getPk())
+            print('lets go %s' % direction)
         #~ Get the area to go from the player (current area) and the direction
