2c738d16e53ae05886c4abc2fcb5511d2693758f
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-02 23:50:25 +0200

    help added for look command

--- CommandHelp.py
+++ CommandHelp.py
@@ -9,6 +9,8 @@ class CommandHelp(CommandAbstract):
 		print('Available commands:')
 		print('talk <Character name> "<Sentence>": Talk to a character')
 		print('move <south|east|west|north>: Go to the indicated direction')
+		print('look: See what is in the current area' +
+			' (characters, items, neighbour areas...)')
 		print('createPlayer: Not Yet Implemented')
 		print('help: Display this help')
 		print('quit|exit|q: Quit the game')
