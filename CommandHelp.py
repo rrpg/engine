0719b5e6d462ea924202cbb70d2e881f4276ce8d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-02 22:30:17 +0200

    this is implemented now

--- CommandHelp.py
+++ CommandHelp.py
@@ -7,8 +7,7 @@ from CommandException import CommandException
 class CommandHelp(CommandAbstract):
     def run(self):
         print('Available commands:')
-        print('talk <Character name> "<Sentence>": Talk to a character' +
-            ' - Not Yet Implemented')
+        print('talk <Character name> "<Sentence>": Talk to a character')
         print('createPlayer: Not Yet Implemented')
         print('help: Display this help')
         print('quit|exit|q: Quit the game')
