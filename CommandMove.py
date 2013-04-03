d2070469a77811e16cc15bc7dd70d7e1390ad4bb
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-03 13:43:24 +0200

    method forgotten

--- CommandMove.py
+++ CommandMove.py
@@ -14,7 +14,7 @@ class CommandMove(CommandAbstract):
 		if direction not in ("north", "east", "south", "west"):
 			raise CommandException("%s is not a valid direction" % direction)
 
-		area = Area.getByIdCHaracterAndDirection(
+		area = Area.getByIdCaracterAndDirection(
 			self._player._model.getIdCharacter(), direction
 		)
 
