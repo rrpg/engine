8919969f6b8369cb38ae7351853539252096a36d
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-03 20:33:58 +0200

    this method was used but not created, design for factorization purposes

--- Character.py
+++ Character.py
@@ -6,25 +6,29 @@ from CharacterModel import CharacterModel
 class Character:
 	@staticmethod
 	def searchByNameAndPlayer(name, player):
-		model = CharacterModel.loadByNameAndIdPlayer(
+		m = CharacterModel.loadByNameAndIdPlayer(
 			name, player.getModel().getPk()
 		)
+		return Character.loadFromModel(m)
 
+	@staticmethod
+	def searchByPlayer(player):
+		models = CharacterModel.loadNeighboursFromIdCharacter(
+			player.getModel().getIdCharacter()
+		)
+		chars = list()
+		for m in models:
+			chars.append(Character.loadFromModel(m))
+		return chars
+
+	@staticmethod
+	def loadFromModel(model):
 		if model is None:
 			return None
 		else:
 			character = Character()
 			character._model = model
 			return character
-
-	@staticmethod
-	def searchByPlayer(player):
-		models = CharacterModel.loadNeighboursFromIdCharacter(
-			player.getModel().getIdCharacter())
-		chars = list()
-		for m in models:
-			chars.append(Character.loadFromModel(m))
-		return chars
 
 	def getId(self):
 		return self._model.getPk()
