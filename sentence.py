5c5b10ee5d52958e2ef2fff416592227192f78cd
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-04 23:02:55 +0200

    SENTENCES - a model is now just a dict

--- sentence.py
+++ sentence.py
@@ -18,7 +18,7 @@ class sentence:
 		return sentences
 
 	def getSentence(self):
-		return self._model.getSentence()
+		return self._model['sentence']
 
 
 class model(Model):
@@ -44,33 +44,4 @@ class model(Model):
 			ORDER BY RANDOM()\
 			"
 
-		sentences = Model.fetchAllRows(query, (triggerWord, idCharacter))
-
-		if len(sentences) > 0:
-			for index, sentence in enumerate(sentences):
-				sentencesModels.append(model())
-				sentencesModels[index]._setPk(
-					sentences[index]['id_talk_answer'])
-				sentencesModels[index].setWord(
-					sentences[index]['trigger_word'])
-				sentencesModels[index].setSentence(
-					sentences[index]['sentence'])
-				sentencesModels[index].setCondition(
-					sentences[index]['condition'])
-
-		return sentencesModels
-
-	def setWord(self, word):
-		self._sentenceFields["word"] = word
-
-	def setSentence(self, sentence):
-		self._sentenceFields["sentence"] = sentence
-
-	def setCondition(self, condition):
-		self._sentenceFields["condition"] = condition
-
-	def _setPk(self, pk):
-		self._sentenceFields["id_talk_answer"] = pk
-
-	def getSentence(self):
-		return self._sentenceFields["sentence"]
+		return Model.fetchAllRows(query, (triggerWord, idCharacter))
