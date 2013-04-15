9557ccf2b8944ce1441b87532a9773b812167ab6
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-04-15 22:51:40 +0200

    documentation for the genders module added

--- gender.py
+++ gender.py
@@ -1,7 +1,16 @@
 # -*- coding: utf8 -*-
 
+"""
+Module to handle the characters's genders in the game.
+For the moment, only a dual genders system is done.
+"""
+
 from Model import Model
 
 
 class model(Model):
+	"""
+	Model class to interact with the genders in the database.
+	"""
+
 	fields = ['id_gender', 'name']
