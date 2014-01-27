95f85b40670e8d8680455f9540b4f8db12bd59e5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-01-27 15:51:21 +0000

    some data added

--- database/values.sql
+++ database/values.sql
@@ -7,8 +7,12 @@ INSERT INTO item (name, weight, flags, effects) VALUES ('Heavy breastplate', 5, 
 INSERT INTO item (name, weight, flags, effects) VALUES ('Mist potion', .1, 6, '{"stealth": 7}');
 
 INSERT INTO region (region_name) VALUES ('Balmora');
-INSERT INTO area (id_region, x, y, items) VALUES (1, 0, 0, '{"1": {"quantity": 1}, "2": {"quantity": 4}}');
-INSERT INTO area (id_region, x, y) VALUES (1, 0, 1);
+INSERT INTO area_type (name) VALUES ('land');
+INSERT INTO area_type (name) VALUES ('dungeon');
+INSERT INTO area (id_region, id_area_type, container, x, y, items) VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 0, '{"1": {"quantity": 1}, "2": {"quantity": 4}}');
+INSERT INTO area (id_region, id_area_type, container, x, y) VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 1);
+
+INSERT INTO place (id_area, id_area_type, name) VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'dungeon'), 'first dungeon');
 
 INSERT INTO `character` (name, id_species, id_gender, id_area) VALUES ('Tom', 1, 1, 2);
 INSERT INTO talk_answer (trigger_word, sentence, condition) VALUES ('hi', "Hi, my name is Tom, I'm a butcher", '{"met":0}');
