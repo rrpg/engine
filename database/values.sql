7a22b3b155765e729c17a2fc130ad4871f6423f7
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-03-24 16:32:21 +0000

    item added in the second area

--- database/values.sql
+++ database/values.sql
@@ -11,8 +11,8 @@ INSERT INTO area_type (name) VALUES ('land');
 INSERT INTO area_type (name) VALUES ('dungeon');
 INSERT INTO area (id_region, id_area_type, container, x, y, directions, items)
 	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 0, 4, '{"1": {"quantity": 1}, "2": {"quantity": 4}}');
-INSERT INTO area (id_region, id_area_type, container, x, y, directions)
-	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 1, 1);
+INSERT INTO area (id_region, id_area_type, container, x, y, directions, items)
+	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 1, 1, '{"1": {"quantity": 2}}');
 
 INSERT INTO settings (key, value) VALUES ('START_CELL_ID', 1);
 
