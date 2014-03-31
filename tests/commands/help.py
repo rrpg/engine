e794cb23de48ce9a843922c436dcef6a62726e47
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-03-31 21:01:35 +0100

    po keys used in the look command tests

--- tests/commands/help.py
+++ tests/commands/help.py
@@ -6,41 +6,76 @@ sys.path.append(os.path.realpath(__file__ + "/../../../"))
 
 import tests.common
 from tests.output_capturer import capturer
+from core.localisation import _
+from models import area, place
+import json
 
 
 class helpTests(tests.common.common):
 	def test_text(self):
-		self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['help'])
+		self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('HELP_COMMAND')])
 		with capturer() as output:
 			self.rpgText._runAction()
-		self.assertTrue(output == [
-			'Available commands',
-			'talk <character name> "<sentence>":',
-			'\tTalk to a character',
-			'move <west|east|north|south>:',
-			'\tGo to the indicated direction',
-			'enter <dungeon|cave>:',
-			'\tEnter in the selected place (if the place is available in the current cell)',
-			'exit <dungeon|cave>:',
-			'\tExit from the selected place (if the current cell is the place exit)',
-			'look:',
-			'\tList everything which is in the current area (characters, items, neighbour areas...)',
-			'inv|inventory:',
-			'\tList the items the player has in his inventory',
-			'take [<quantity>] "<item name>":',
-			'\tTake some items on the ground',
-			'drop [<quantity>] "<item name>":',
-			'\tDrop some items from your inventory',
-			'help:',
-			'\tDisplay the help menu',
-			'q|quit:',
-			'\tQuit the game'
-		])
+		expected = [
+			_('AVAILABLE_COMMANDS_TITLE'),
+			'%s <%s> "<%s>"' % (_('TALK_COMMAND'), _('CHARACTER_TOKEN'), _('SENTENCE_TOKEN')) + ':',
+			'\t' + _('TALK_COMMAND_DESCRIPTION'),
+			'%s <%s>' % (_('MOVE_COMMAND'), '|'.join([str(s) for s in area.directions])) + ':',
+			'\t' + _('MOVE_COMMAND_DESCRIPTION'),
+			'%s <%s>' % (_('ENTER_COMMAND'), '|'.join([str(s) for s in place.types])) + ':',
+			'\t' + _('ENTER_COMMAND_DESCRIPTION'),
+			'%s <%s>' % (_('EXIT_COMMAND'), '|'.join([str(s) for s in place.types])) + ':',
+			'\t' + _('EXIT_COMMAND_DESCRIPTION'),
+			_('LOOK_COMMAND') + ':',
+			'\t' + _('LOOK_COMMAND_DESCRIPTION'),
+			_('INVENTORY_SHORT_COMMAND') + '|' + _('INVENTORY_COMMAND') + ':',
+			'\t' + _('INVENTORY_COMMAND_DESCRIPTION'),
+			'%s [<%s>] "<%s>"' % (_('TAKE_COMMAND'), _('QUANTITY_TOKEN'), _('ITEM_NAME_TOKEN')) + ':',
+			'\t' + _('TAKE_COMMAND_DESCRIPTION'),
+			'%s [<%s>] "<%s>"' % (_('DROP_COMMAND'), _('QUANTITY_TOKEN'), _('ITEM_NAME_TOKEN')) + ':',
+			'\t' + _('DROP_COMMAND_DESCRIPTION'),
+			_('HELP_COMMAND') + ':',
+			'\t' + _('HELP_COMMAND_DESCRIPTION'),
+			_('QUIT_SHORT_COMMAND') + '|' + _('QUIT_COMMAND') + ':',
+			'\t' + _('QUIT_COMMAND_DESCRIPTION')
+		]
+		self.assertTrue(output == expected)
 
 	def test_json(self):
-		self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['help'])
+		self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('HELP_COMMAND')])
 		with capturer() as output:
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['[{"command": "talk <character name> \\"<sentence>\\"", "description": "Talk to a character"}, {"command": "move <west|east|north|south>", "description": "Go to the indicated direction"}, {"command": "enter <dungeon|cave>", "description": "Enter in the selected place (if the place is available in the current cell)"}, {"command": "exit <dungeon|cave>", "description": "Exit from the selected place (if the current cell is the place exit)"}, {"command": "look", "description": "List everything which is in the current area (characters, items, neighbour areas...)"}, {"command": "inv|inventory", "description": "List the items the player has in his inventory"}, {"command": "take [<quantity>] \\"<item name>\\"", "description": "Take some items on the ground"}, {"command": "drop [<quantity>] \\"<item name>\\"", "description": "Drop some items from your inventory"}, {"command": "help", "description": "Display the help menu"}, {"command": "q|quit", "description": "Quit the game"}]'])
+		expected = '[{"command": "' \
+			+ ('%s <%s> \\"<%s>\\"' % (_('TALK_COMMAND'), _('CHARACTER_TOKEN'), _('SENTENCE_TOKEN'))) \
+			+ '", "description": "' + _('TALK_COMMAND_DESCRIPTION') \
+			+ '"}, {"command": "' \
+			+ ('%s <%s>' % (_('MOVE_COMMAND'), '|'.join([str(s) for s in area.directions]))) \
+			+ '", "description": "' + _('MOVE_COMMAND_DESCRIPTION') \
+			+ '"}, {"command": "'\
+			+ ('%s <%s>' % (_('ENTER_COMMAND'), '|'.join([str(s) for s in place.types])))\
+			+ '", "description": "' + _('ENTER_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "'\
+			+ ('%s <%s>' % (_('EXIT_COMMAND'), '|'.join([str(s) for s in place.types])))\
+			+ '", "description": "' + _('EXIT_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "' + _('LOOK_COMMAND')\
+			+ '", "description": "' + _('LOOK_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "'\
+			+ _('INVENTORY_SHORT_COMMAND') + '|' + _('INVENTORY_COMMAND')\
+			+ '", "description": "' + _('INVENTORY_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "'\
+			+ ('%s [<%s>] \\"<%s>\\"' % (_('TAKE_COMMAND'), _('QUANTITY_TOKEN'), _('ITEM_NAME_TOKEN')))\
+			+ '", "description": "' + _('TAKE_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "'\
+			+ ('%s [<%s>] \\"<%s>\\"' % (_('DROP_COMMAND'), _('QUANTITY_TOKEN'), _('ITEM_NAME_TOKEN')))\
+			+ '", "description": "' + _('DROP_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "' + _('HELP_COMMAND')\
+			+ '", "description": "' + _('HELP_COMMAND_DESCRIPTION')\
+			+ '"}, {"command": "'\
+			+ _('QUIT_SHORT_COMMAND') + '|' + _('QUIT_COMMAND')\
+			+ '", "description": "' + _('QUIT_COMMAND_DESCRIPTION') + '"}]'
 
+		output = json.loads(output[0])
+		expected = json.loads(expected)
+
+		self.assertTrue(output == expected)
 unittest.main()
