b05d12754a09bb0dfc7eba65f651ca7ff7d53425
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-03-31 22:03:11 +0100

    gettext keys used for the enter command

--- tests/commands/enter.py
+++ tests/commands/enter.py
@@ -6,73 +6,74 @@ sys.path.append(os.path.realpath(__file__ + "/../../../"))
 
 import tests.common
 from tests.output_capturer import capturer
+from core.localisation import _
 
 
 class enterTests(tests.common.common):
 	def test_no_place_given_text(self):
 		with capturer() as output:
-			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter'])
+			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND')])
 			self.rpgText._runAction()
-		self.assertTrue(output == ['I can\'t enter into nothing'])
+		self.assertTrue(output == [_('ERROR_ENTER_NO_PLACE_GIVEN')])
 
 	def test_no_place_given_json(self):
 		with capturer() as output:
-			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter'])
+			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND')])
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['{"error": {"message": "I can\'t enter into nothing", "code": 1}}'])
+		self.assertTrue(output == ['{"error": {"message": "' + _('ERROR_ENTER_NO_PLACE_GIVEN') + '", "code": 1}}'])
 
 	def test_place_not_available_text(self):
 		with capturer() as output:
-			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'cave'])
+			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), _('PLACE_TYPE_CAVE')])
 			self.rpgText._runAction()
-		self.assertTrue(output == ['There is no such place here'])
+		self.assertTrue(output == [_('ERROR_ENTER_PLACE_NOT_AVAILABLE')])
 
 	def test_no_place_given_json(self):
 		with capturer() as output:
-			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'cave'])
+			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), 'cave'])
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['{"error": {"message": "There is no such place here", "code": 1}}'])
+		self.assertTrue(output == ['{"error": {"message": "' + _('ERROR_ENTER_PLACE_NOT_AVAILABLE') + '", "code": 1}}'])
 
 	def test_wrong_place_text(self):
 		with capturer() as output:
-			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dummyplace'])
+			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), 'dummyplace'])
 			self.rpgText._runAction()
-		self.assertTrue(output == ['Unknown place type'])
+		self.assertTrue(output == [_('ERROR_UNKNOWN_PLACE_TYPE')])
 
 	def test_wrong_place_given_json(self):
 		with capturer() as output:
-			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dummyplace'])
+			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), 'dummyplace'])
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['{"error": {"message": "Unknown place type", "code": 1}}'])
+		self.assertTrue(output == ['{"error": {"message": "' + _('ERROR_UNKNOWN_PLACE_TYPE') + '", "code": 1}}'])
 
 	def test_when_already_in_text(self):
-		self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dungeon'])
+		self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 		with capturer() as output:
 			self.rpgText._runAction()
-		self.rpgText.setAction(['enter', 'dungeon'])
+		self.rpgText.setAction([_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 		with capturer() as output:
 			self.rpgText._runAction()
-		self.assertTrue(output == ['There is no such place here'])
+		self.assertTrue(output == [_('ERROR_ENTER_PLACE_NOT_AVAILABLE')])
 
 	def test_when_already_in_json(self):
-		self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dungeon'])
+		self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 		with capturer() as output:
 			self.rpgJSON._runAction()
-		self.rpgJSON.setAction(['enter', 'dungeon'])
+		self.rpgJSON.setAction([_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 		with capturer() as output:
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['{"error": {"message": "There is no such place here", "code": 1}}'])
+		self.assertTrue(output == ['{"error": {"message": "' + _('ERROR_ENTER_PLACE_NOT_AVAILABLE') + '", "code": 1}}'])
 
 	def test_text(self):
 		with capturer() as output:
-			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dungeon'])
+			self.rpgText.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 			self.rpgText._runAction()
-		self.assertTrue(output == ['You enter.'])
+		self.assertTrue(output == [_('ENTER_CONFIRMATION')])
 
 	def test_json(self):
 		with capturer() as output:
-			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', ['enter', 'dungeon'])
+			self.rpgJSON.init(self.dbFile, 'TEST_PLAYER', 'TEST_PLAYER', [_('ENTER_COMMAND'), _('PLACE_TYPE_DUNGEON')])
 			self.rpgJSON._runAction()
-		self.assertTrue(output == ['["You enter."]'])
+		self.assertTrue(output == ['["' + _('ENTER_CONFIRMATION') + '"]'])
 
 unittest.main()
