26058a66dad52ea77e0e9afaf351151068e09057
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-02 21:56:49 +0100

    python3 tests added

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -8,3 +8,12 @@ python tests/commands/enter.py
 python tests/commands/exit.py
 python tests/commands/help.py
 python tests/commands/inventory.py
+
+python3 tests/commands/look.py
+python3 tests/commands/take.py
+python3 tests/commands/drop.py
+python3 tests/commands/move.py
+python3 tests/commands/enter.py
+python3 tests/commands/exit.py
+python3 tests/commands/help.py
+python3 tests/commands/inventory.py
