93150a1285c3083f25bc316282cec9f6c4226e32
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-16 22:55:43 +0100

    skeleton of the open command's tests added

---
+++ tests/commands/testOpen.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import unittest
+import sys
+import os
+sys.path.append(os.path.realpath(__file__ + "/../../../"))
+
+import tests.common
+from core.localisation import _
+import json
+
+
+class openTests(tests.common.common):
+	def test_no_container_given_text(self):
+		pass
+
+	def test_no_container_given_json(self):
+		pass
+
+	def test_container_not_found_here_text(self):
+		pass
+
+	def test_container_not_found_here_json(self):
+		pass
+
+	def test_unknown_container_type_text(self):
+		pass
+
+	def test_unknown_container_type_json(self):
+		pass
+
+	def test_multiple_container_of_given_type_text(self):
+		pass
+
+	def test_multiple_container_of_given_type_json(self):
+		pass
+
+	def test_text(self):
+		pass
+
+	def test_json(self):
+		pass
