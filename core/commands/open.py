3d001e5312236e19ec087e377ba9ec0308c18ef5
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-18 22:31:23 +0100

    basic open command implemented

--- core/commands/open.py
+++ core/commands/open.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from models import item_container, item
 import core.command
 from core.localisation import _
 
@@ -14,7 +15,29 @@ class open(core.command.command):
 		if len(self._args) == 0:
 			raise core.command.exception(_('ERROR_OPEN_NO_CONTAINER_PROVIDED'))
 
-		return []
+		containers = item_container.factory.getAllFromIdAreaAndType(
+			self._player.getAreaId(),
+			self._args[0]
+		)
+
+		if len(containers) > 1:
+			raise core.command.exception(_('ERROR_OPEN_MULTIPLE_CONTAINERS_AVAILABLE'))
+
+		items = item.inventory.fromStr(containers[0]['items'])
+
+		result = {'container_type': self._args[0], 'items': list()}
+		for i in items:
+			it = item.model.loadById(i)
+			result['items'].append({
+				'name': it['name'],
+				'quantity': items[i]['quantity']
+			})
+
+		return result
 
 	def render(self, data):
-		return ''
+		output = list()
+		output.append(_('ITEMS_IN_CONTAINER_%s') % data['container_type'])
+		for i in data['items']:
+			output.append(str(i['quantity']).rjust(3) + ' ' + i['name'])
+		return '\n'.join(output)
