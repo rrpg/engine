6bb026ecacb522faebcb24bf75d4f4579ae6a139
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2014-04-29 12:42:41 +0100

    code set and checked in the exception class

--- core/exception.py
+++ core/exception.py
@@ -1,4 +1,11 @@
 # -*- coding: utf-8 -*-
 
 class exception(BaseException):
-	code = 1
+	def __init__(self, message='', code=1):
+		BaseException.__init__(self, message)
+		if code not in self.getCodes():
+			raise ValueError('Unknown error code')
+		self.code = code
+
+	def getCodes(self):
+		return (1,)
