dde06a79109007ae8886c2d3283f669986df3dcc
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-02-11 07:58:46 +0000

    Creature model added

---
+++ models/creature.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+"""
+Module to handle the enemies to fight
+"""
+
+from models.Model import Model
+
+
+class creature:
+	"""
+	Class to interact with the creatures in the game.
+	"""
+
+	@staticmethod
+	def getFromAreaType(idAreaType):
+		c = model.getFromAreaType(idAreaType)
+		if c == {}:
+			return None
+
+		return c
+
+
+class model(Model):
+	"""
+	Class to interact with the values in the database.
+	"""
+
+	fields = (
+		'id_creature', 'name'
+	)
+
+	@staticmethod
+	def getFromAreaType(idAreaType):
+		"""
+		emeny.model.getFromArea(area) -> dict()
+
+		Return a random enemy that the player can encounter in a given
+		area type
+
+		@param area dict area where the enemy can be found
+
+		@return dict an enemy or None if no enemy can be found in the
+		given area type
+		"""
+		query = "\
+			SELECT\
+				name,\
+				stats_hp,\
+				stat_strength,\
+				stat_defence,\
+				stat_speed,\
+				stat_accuracy\
+			FROM\
+				creature\
+				JOIN creature_area_type ON creature.id_creature = creature_area_type.id_creature\
+			WHERE\
+				creature_area_type.id_area_type = ?\
+			ORDER BY RANDOM() LIMIT 1\
+		"
+
+		return Model.fetchOneRow(query, [idAreaType])
