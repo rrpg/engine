2129e1595fe2ef718dd9ca1f2bac50eaf01ede39
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-02-11 22:27:08 +0000

    module completed

--- core/fight.py
+++ core/fight.py
@@ -2,5 +2,21 @@
 
 # module to handle fights
 
+import core.registry
+import core.exception
+
+
 def startFight(player, enemy):
 	player.fight(True)
+	core.registry.set('fight_enemy', enemy)
+
+def getEnemy():
+	return core.registry.get('fight_enemy')
+
+def stopFight(player):
+	player.fight(False)
+	core.registry.set('fight_enemy', None)
+
+
+class exception(core.exception.exception):
+	pass
