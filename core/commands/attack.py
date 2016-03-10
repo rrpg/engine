b36524b432354e796bfd5af276e9e5a3a9ea1540
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-10 22:59:59 +0000

    basic and dirty fight turn implemented

--- core/commands/attack.py
+++ core/commands/attack.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from models import character, sentence
-import random
 import core.command
+import core.fight
 from core.localisation import _
 
 class attack(core.command.command):
@@ -18,8 +17,20 @@ class attack(core.command.command):
 			raise core.command.exception(_('ERROR_FIGHT_NO_TARGET_GIVEN'))
 
 		targetName = self._args[0]
+		enemy = core.fight.getEnemy()
+		attackResult = core.fight.attack(
+			self._player, enemy
+		)
 
-		return {'name': targetName}
+		return {
+			'enemy': enemy,
+			'attackResult': attackResult
+		}
 
 	def render(self, data):
-		return 'You attack ' + data['name']
+		output = ['You attack ' + data['enemy']['name'] + ' and deal ' + str(data['attackResult']['damagesToEnemy']) + ' points of damage']
+
+		if data['attackResult']['fightFinished']:
+			output.append('The fight is over')
+
+		return '\n'.join(output)
