5e50a7e1226c7e6ab4214e0bbeab38a9956af409
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-15 22:04:41 +0000

    tests for the settings class aded

---
+++ tests/models/testSettings.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+import sys
+import os
+sys.path.append(os.path.realpath(__file__ + "/../../../"))
+
+import tests.common
+from core.localisation import _
+import models.settings
+import json
+
+
+class settingsTests(tests.common.common):
+	def test_unknown_setting(self):
+		thrownException = None
+		try:
+			models.settings.settings.get('foo')
+		except IndexError as e:
+			thrownException = str(e)
+
+		self.assertEquals(thrownException, "Unknown key foo in the settings")
+
+	def test_ok(self):
+		s = models.settings.settings.get('START_CELL_ID')
+		self.assertEquals(s, '1')
