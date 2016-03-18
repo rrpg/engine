0ed5cdc0a1a44748764efdda726435474cd4dd82
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-18 15:59:13 +0000

    elses removed

--- core/commands/move.py
+++ core/commands/move.py
@@ -32,39 +32,39 @@ class move(core.command.command):
 
 		if area.area.canGoTo(curArea['directions'], direction) is False or a is None:
 			raise core.command.exception(_('ERROR_MOVE_DIRECTION_NOT_AVAILABLE'))
-		else:
-			wasFighting = self._player.isFighting()
-			f = fight.getFight()
-			if wasFighting:
-				enemy = f.enemy
 
-				if not fight.canFlee(self._player._model, enemy):
-					raise core.fight.exception(_('ERROR_FLEE_FIGHT_FAILS'))
-				else:
-					fight.stopFight()
-					ret['flee'] = True
-					ret['enemy'] = enemy['name']
+		wasFighting = self._player.isFighting()
+		f = fight.getFight()
+		if wasFighting:
+			enemy = f.enemy
 
-			self._player.goTo(a._model['id_area'])
+			if not fight.canFlee(self._player._model, enemy):
+				raise core.fight.exception(_('ERROR_FLEE_FIGHT_FAILS'))
 
-			# let's be fair, if the player succesfully ran away from a
-			# fight, he probably does not want to arrive right away in a
-			# new one
-			enemy = None
-			if not wasFighting:
-				probability = randint(0, 1000) / 1000.0
-				enemy = creature.creature.getFromAreaType(
-					a._model['id_area_type'],
-					probability
-				)
+			fight.stopFight()
+			ret['flee'] = True
+			ret['enemy'] = enemy['name']
 
-			if enemy is not None:
-				ret['enemy'] = enemy['name']
-				f = fight.startFight(self._player, enemy)
-				# Deal with enemy attacking first
-				damages = f.enemyTriesToAttackFirst(self._player)
-				if damages is not None:
-					ret['damages'] = damages
+		self._player.goTo(a._model['id_area'])
+
+		# let's be fair, if the player succesfully ran away from a
+		# fight, he probably does not want to arrive right away in a
+		# new one
+		enemy = None
+		if not wasFighting:
+			probability = randint(0, 1000) / 1000.0
+			enemy = creature.creature.getFromAreaType(
+				a._model['id_area_type'],
+				probability
+			)
+
+		if enemy is not None:
+			ret['enemy'] = enemy['name']
+			f = fight.startFight(self._player, enemy)
+			# Deal with enemy attacking first
+			damages = f.enemyTriesToAttackFirst(self._player)
+			if damages is not None:
+				ret['damages'] = damages
 
 		return ret
 
