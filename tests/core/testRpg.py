3374f234ad3c6c10a4de7563ea0b378223815b35
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-21 15:57:18 +0000

    empty test suite for the engine

---
+++ tests/core/testRpg.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+import tests.common
+import core
+from core.localisation import _
+from core import Rpg
+import models.settings
+import json
+
+
+class rpgTests(tests.common.common):
+	pass
