d63ebf9313bf363c0c0171bacfb0c469964dcfdd
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-03-30 23:18:24 +0100

    method to load all the saved games

--- models/saved_game.py
+++ models/saved_game.py
@@ -5,4 +5,32 @@ import sqlite3
 
 
 class saved_game:
-	pass
+	@staticmethod
+	def loadAll():
+		return model.loadAll()
+
+
+class model(Model):
+	"""
+	Class to interact with the values in the database.
+	"""
+
+	fields = (
+		'id_saved_game',
+		'id_player'
+	)
+
+	@staticmethod
+	def loadAll():
+		query = "\
+			SELECT\
+				id_saved_game,\
+				login\
+			FROM\
+				saved_game sg\
+				LEFT JOIN player p\
+					ON sg.id_player = p.id_player\
+			ORDER BY id_saved_game\
+			"
+
+		return Model.fetchAllRows(query)
