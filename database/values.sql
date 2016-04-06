b521fe83dc3fbaff638a3ad21befa6f40915cb08
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-07 00:02:07 +0100

    saved point added in the example world

--- database/values.sql
+++ database/values.sql
@@ -13,8 +13,8 @@ INSERT INTO area_type (name) VALUES ('land');
 INSERT INTO area_type (name) VALUES ('dungeon');
 INSERT INTO area (id_region, id_area_type, container, x, y, directions, items)
 	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 0, 1, '{"1": {"quantity": 1}, "2": {"quantity": 4}}');
-INSERT INTO area (id_region, id_area_type, container, x, y, directions, items)
-	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 1, 4, '{"1": {"quantity": 6}}');
+INSERT INTO area (id_region, id_area_type, container, x, y, directions, items, has_save_point)
+	VALUES (1, (SELECT id_area_type FROM area_type WHERE name = 'land'), 'world', 0, 1, 4, '{"1": {"quantity": 6}}', 'TRUE');
 
 INSERT INTO item_container_type (label) VALUES ('chest');
 INSERT INTO item_container_type (label) VALUES ('box');
