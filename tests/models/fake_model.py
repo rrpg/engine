50c25da85256f761e5ee110a5e20666c7a2f76c9
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2016-04-08 00:31:20 +0100

    dummy model added to test some specific parts of the Model class

---
+++ tests/models/fake_model.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Model used only for the unit tests to test the Model class
+"""
+
+from models.Model import Model
+
+
+class model(Model):
+	fields = ('id', 'field1', 'field2')
